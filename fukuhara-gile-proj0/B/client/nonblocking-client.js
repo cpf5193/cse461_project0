ad2d7f77fe78f20ec7f632ac35ce2b8aa2292ab8
cpf5193 <cpf5193@cs.washington.edu>
2015-01-15 10:43:59 -0800

    Full initial client b implementation

--- fukuhara-gile-proj0/B/client/nonblocking-client.js
+++ fukuhara-gile-proj0/B/client/nonblocking-client.js
@@ -5,6 +5,7 @@ var closing = false;
 var TIMEOUT_DURATION = 5000;
 var HEADER_SIZE = 12;
 var sequenceNum = 0;
+var alivesReceived = 0;
 var sessionId = Math.floor((Math.random() * 2147483647)).toString(2);
 
 // Get the host and port number from the command line arguments
@@ -27,13 +28,10 @@ clientSocket.send(buf, HEADER_SIZE, 0, serverPort, serverHost, function() {
   // Timeout if no response within TIMEOUT_DURATION milliseconds
   timer = setTimeout(function() {
     console.log("No response from " + serverHost);
+    if (closing) { process.exit(0); }
     timer = null;
 
-    // send a GOODBYE
-    var goodbyeHeader = makeHeaderString(3);
-    clientSocket.send(goodbyeHeader, HEADER_SIZE, 0, serverPort, serverHost, function() {
-      closing = true;
-    });
+    sendGoodbye();
     
   }, TIMEOUT_DURATION);
 });
@@ -49,14 +47,19 @@ clientSocket.on('message', function(message) {
     // HELLO, cancel timer and transition to ready
     timer = null;
   } else if (type == 2) {
-    if (timer != null) {
+    // ALIVE, cancel timer if it is in ready state with timer set
+    if (timer != null && !closing) {
       timer = null;
     }
+    alivesReceived++;
   } else {
+    // Goodbye
     if (!closing) {
-      console.log("server shut down. Closing connection.");
+      // Received a GOODBYE in an unexpected state
+      console.log("Server shut down. Closing connection.");
       process.exit(1);
     } else {
+      console.log("Connection closed.");
       process.exit(0);
     }
   }
@@ -66,6 +69,10 @@ clientSocket.on('message', function(message) {
 // Register handler for user input
 /////////////////////////////////////////////
 process.stdin.on('data', function(dataPiece) {
+  var input = dataPiece.toString().trim();
+  if (input == "q") {
+    sendGoodbye();
+  }
   var data = makeHeaderString(1) + dataPiece.toString().trim();
   var message = new Buffer(data);
 
@@ -80,25 +87,25 @@ process.stdin.on('data', function(dataPiece) {
   if (timer == null) {
     timer = setTimeout(function() {
       console.log("No response to DATA from server");
+      if (closing) { process.exit(0); }
       timer = null;
   
-      // send a GOODBYE
-      var goodbyeHeader = makeHeaderString(3);
-      clientSocket.send(goodbyeHeader, HEADER_SIZE, 0, serverPort, serverHost, function() {
-        closing = true;
-      });
+      sendGoodbye();
   
     }, 5000);
   }
 });
 
 process.stdin.on('end', function() {
+  // Try to wait until all outgoing messages have been put on the network
+  // We will wait at most 20 seconds
+  var tries = 0;
+  console.log("closing connection...");
+  while(alivesReceived != sequenceNum && tries < 20) {
+    setTimeout(function() {}, 1000);
+  }
   console.log("eof");
-  // send a GOODBYE
-  var goodbyeHeader = makeHeaderString(3);
-  clientSocket.send(goodbyeHeader, HEADER_SIZE, 0, serverPort, serverHost, function() {
-    closing = true;
-  });
+  sendGoodbye();
 });
 
 //////////////////////////////////////////
@@ -122,4 +129,12 @@ function makeHeaderString(requestType) {
     binarySequence = "0" + binarySequence;
   }
   return magic + version + command + binarySequence + sessionId;
+}
+
+function sendGoodbye() {
+  // send a GOODBYE to the server
+  var goodbyeHeader = makeHeaderString(3);
+  clientSocket.send(goodbyeHeader, HEADER_SIZE, 0, serverPort, serverHost, function() {
+    closing = true;
+  });
 }
