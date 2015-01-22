e54e66abf3ad42f4f6f47a66538528ba9a240ad8
jjgile <jjgile@cs.washington.edu>
2015-01-21 16:35:48 -0800

    Functioning part A

--- fukuhara-gile-proj0/A/client/threaded-client.py
+++ fukuhara-gile-proj0/A/client/threaded-client.py
@@ -1,6 +1,4 @@
-import sys
-import socket
-import threading
+import sys, socket, threading, os
 from struct import pack, unpack
 from collections import namedtuple
 from binascii import hexlify
@@ -14,42 +12,123 @@ DATA = 1
 ALIVE = 2
 GOODBYE = 3
 HEADER_FORMAT = '!HbbII'
-HEADER_SIZE = 96
+HEADER_SIZE = 12
 MESSAGE_SIZE = 1024
 MAX_ID = 0xFFFFFFFF
 MIN_ID = 0x00000000
+TIMEOUT = 10.0
+
+sequence = 0;
+sessionId = randint(MIN_ID, MAX_ID);
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+def incrementSequence():
+	global sequence
+	sequence += 1
+
+def endSession():
+	debug("endSession()");
+	sendGoodbye()
+	os._exit(0);
+	
+def restartTimer():
+	global timer
+	debug("restartTimer()");
+	timer = threading.Timer(TIMEOUT, endSession)
+	timer.start()
 
 def header(cmd, seq, id):
 	return pack(HEADER_FORMAT, MAGIC, VERSION, cmd , seq, id);
 
 def debug(message):
 	if(DEBUG_LEVEL > 0):
-		print(message)
+		print("DEBUG::" + str(message))
+		
+def validateHeader(msg):
+	(magic, version, cmd, seq, id) = unpack(HEADER_FORMAT, msg)
+	debug(msg)
+	if(magic != MAGIC or version != VERSION or id != sessionId):
+		return -1
+	return cmd
+	
+def receiveMessage():
+	msg = sock.recv(HEADER_SIZE)
+	debug("Message length: " +  str(len(msg)))
+	cmd = validateHeader(msg)
+	if(len(msg) != HEADER_SIZE or cmd < 0):
+		debug("Improperly formatted header")
+		endSession();
+	return cmd
+
+
 
 def main():
 	host = sys.argv[1]
 	port = int(sys.argv[2])
 	
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
 	sock.connect((host, port));
+	
+	debug("Connected to " + host + " " + str(port));
+
+
+	stdinThread = threading.Thread(target=readStdin, args=())
+	stdinThread.start()
+		
+	sendHello()
 		
 	debug("Speaking to %s:%d" % (host, port))
 	debug("Example header: %s" % hexlify(header(1, 2, MAX_ID)));
 	
-	startSession(sock)
-	
-def getSessionId():
-	return randint(MIN_ID, MAX_ID);
-	
+	try:
+		while True:
+			msg = receiveMessage();
+			if(msg is ALIVE):
+				timer.cancel()
+			else:
+				endSession()
+	except KeyboardInterrupt:
+		debug("KeyboardInterrupt\n")
+		endSession()
+		
+		
+
+		
 def prependHeader(cmd, seq, id, message):
 	return "%s%s" % (header(cmd, seq, id), message)
 	
-def startSession(s):
-	seq = 1
-	sessId = getSessionId();
-	debug(str(sessId))
-	m = header(HELLO, seq, sessId);
-	s.send(m);
+
+def sendData(payload):
+	sock.send(prependHeader(DATA, sequence, sessionId, payload));
+	incrementSequence()
+	
+def sendHello():
+	sock.send(header(HELLO, sequence, sessionId));
+	restartTimer();
+	msg = receiveMessage();
+	if(msg != HELLO):
+		endSession()
+	timer.cancel()
+	incrementSequence()
+
+def sendGoodbye():
+	sock.send(header(GOODBYE, sequence, sessionId))
+
+def readStdin():
+	while True:
+		line = sys.stdin.readline();
+		if(not line):
+			debug("Read EOF")
+			endSession()
+		elif(line.strip() is 'q'):
+			debug("Read q")
+			endSession()
+		elif(sequence < 1):
+			continue
+		else:
+			debug("Read '" + line.strip() + "'");
+			if(not timer.isAlive()):
+				restartTimer();
+			sendData(line.strip());
 
 if __name__ == "__main__":
 	main()
---
+++ fukuhara-gile-proj0/A/server/eventloop-server.js
@@ -0,0 +1,237 @@
+if (process.argv.length != 3) {
+  console.log("Usage: ./server <port> (using script)");
+  process.exit(1);
+}
+
+var datagram = require('dgram');
+var readline = require('readline');
+var tty = require('tty');
+
+var DEBUG_LEVEL = 1
+var MAGIC = 0xC461
+var MAGIC_OFFSET = 0
+var MAGIC_LENGTH = 2
+var VERSION = 1
+var VERSION_OFFSET = MAGIC_LENGTH
+var VERSION_LENGTH = 1
+var HELLO = 0
+var DATA = 1
+var ALIVE = 2
+var GOODBYE = 3
+var COMMAND_OFFSET = VERSION_OFFSET + VERSION_LENGTH
+var COMMAND_LENGTH = 1;
+var SEQUENCE_OFFSET = COMMAND_OFFSET + COMMAND_LENGTH
+var SEQUENCE_LENGTH = 4
+var SESSION_OFFSET = SEQUENCE_OFFSET + SEQUENCE_LENGTH
+var SESSION_LENGTH = 4
+var HEADER_SIZE = SESSION_OFFSET + SESSION_LENGTH
+var MESSAGE_SIZE = 1024
+var TIMEOUT_DURATION = 20000;
+
+
+var sessions = new Array();
+var timers = new Array();
+var remotes = new Array();
+
+var portNum = process.argv[2];
+
+var port = datagram.createSocket('udp4');
+port.bind(portNum)
+
+console.log("Waiting on port " + portNum + "...");
+
+function debug(msg) {
+	if(DEBUG_LEVEL > 0) {
+		console.log("DEBUG::" + msg);
+	}
+}
+
+process.stdin.on('readable', function() {
+	var input = process.stdin.read();
+	if(input !== null && input.toString().trim() === 'q') {
+		quit(0);
+	}
+});
+
+
+process.stdin.on('end', function() {
+	quit(0);
+});
+
+
+function Message(m) {
+	this.magicNum = m.readUInt16BE(MAGIC_OFFSET);
+	this.version = m.readUInt8(VERSION_OFFSET);
+	this.command = m.readUInt8(COMMAND_OFFSET);
+	this.seq = m.readUInt32BE(SEQUENCE_OFFSET);
+	this.id = m.readUInt32BE(SESSION_OFFSET);
+	this.payload = m.slice(HEADER_SIZE);
+}
+
+function linePrefix(id, seq) {
+	return "0x" + id.toString(16) + " [" + 
+			seq + "] "
+}
+
+//////////////////////////////////
+// Handle messages from the server
+//////////////////////////////////
+port.on('message', function(msg, rinfo) {
+	if(msg.byteLength < HEADER_SIZE) {
+		console.log("Received packet less than minmum header size");
+		process.exit(1);
+	}
+	 
+	var message = new Message(msg);
+	
+	if(message.magicNum != MAGIC || message.version != 1) {
+		return;
+	}
+	
+	switch(message.command) {
+		case HELLO:
+			processHello(message.id, message.seq, rinfo);
+			break;
+		case DATA:
+			processData(message.id, message.seq, message.payload);
+			break;
+		case GOODBYE:
+			processGoodbye(message.id, message.seq);
+			break;
+		default:
+			quit(1);
+			break;
+	}
+});
+
+function processHello(id, seq, r) {
+	debug("processHello(" + id + ", " + seq + ", " + JSON.stringify(r)
+		+ ")");
+	if(sessionExists(id)) {
+		debug("Unexpected HELLO from " + id);
+		quit(1);
+	} else {
+		startSession(id, r);
+		console.log(linePrefix(id, seq) + "Session created");
+	}
+}
+
+function getSessionSeq(id) {
+	if(sessions[id] > -1) {
+		return sessions[id];
+	}
+	else return undefined;
+}
+
+function resetTimer(id) {
+	debug("resetTimer(" + id + ")");
+	clearTimeout(timers[id]);
+	timers[id] = setTimeout(function() {
+		sendGoodbye(id);
+	}, TIMEOUT_DURATION);
+}
+
+function startSession(id, r) {
+	sessions[id] = 0;
+	remotes[id] = r;
+	sendHello(id)
+	resetTimer(id);
+}
+
+function endSession(id) {
+	delete sessions[id];
+	delete remotes[id];
+	clearTimeout(timers[id]);
+	delete timers[id];
+}
+
+function sessionExists(id) {
+	return getSessionSeq(id) !== undefined;
+}
+
+function processData(id, seq, payload) {
+	debug("processData(" + id + " , " + seq + ", "+ payload + ")");
+	if(!sessionExists(id)) {
+		debug("Unepected DATA from " + id);
+		quit(0);
+	}
+	else {
+		var lastSeq = getSessionSeq(id);
+		if (lastSeq < seq - 1) {
+			sendGoodbye(id);
+		} else {
+			sendAlive(id);
+			if (lastSeq > seq - 1) {
+				printLostPackets(id, lastSeq, seq, payload);
+			}
+			console.log(linePrefix(id, seq) + payload);
+		}
+	}
+}
+
+
+function processGoodbye(id, seq) {
+	debug("processGoodbye(" + id + ", " + seq + ")");
+	if(!sessionExists(id)) {
+		debug("Unexpected GOODBYE form " + id);
+		quit(1);
+	}
+	sendGoodbye(id);
+}
+
+function sendGoodbye(id) {
+	sendMessage(GOODBYE, id);
+	endSession(id);
+	console.log(goodbyePrefix(id), "Session closed");
+	debug("Timers left: " + Object.keys(timers).length);
+	debug("Sessions left: " + Object.keys(sessions).length);
+	debug("remotes left: " + Object.keys(remotes).length);
+}
+
+function sendHello(id) {
+	sendMessage(HELLO, id);
+}
+
+function sendAlive(id) {
+	resetTimer(id);
+	sessions[id]++;
+	sendMessage(ALIVE, id);
+}
+
+function goodbyePrefix(id) {
+	return "0x" + id.toString(16);
+}
+
+function linePrefix(id, seq) {
+	return "0x" + id.toString(16) + " [" + seq + "] ";
+}
+
+function printLostPackets(id, start, end, payload) {
+	for(var i = start; i < end; i++) {
+		Console.log(linePrefix(id, i) + "Lost packet!");
+	}
+}
+
+function quit(errno) {
+	for(var id in Object.keys(sessions)) {
+		sendGoodbye(id);
+	}
+	process.exit(errno);
+}
+
+function Header(command, id) {
+	var header = new Buffer(HEADER_SIZE);
+	header.writeUInt16BE(MAGIC, MAGIC_OFFSET);
+	header.writeUInt8(VERSION, VERSION_OFFSET);
+	header.writeUInt8(command, COMMAND_OFFSET);
+	header.writeUInt32BE(0, SEQUENCE_OFFSET);
+	header.writeUInt32BE(id, SESSION_OFFSET);
+	return header;
+}
+
+function sendMessage(command, id) {
+	var head = Header(command, id);
+	debug("Sending: '" + JSON.stringify(new Message(head)) + "'");
+	port.send(head, 0, HEADER_SIZE, remotes[id].port,
+		remotes[id].address);
+}
